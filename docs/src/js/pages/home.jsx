2f328058a9abac2ded4be6bcfaf83c71f9108bc1
jack <1395093509@qq.com>
2015-09-01 15:32:25 +0800

    update

--- docs/src/js/pages/home.jsx
+++ docs/src/js/pages/home.jsx
@@ -12,10 +12,10 @@ export default class Home extends React.Component {
         <div className="hero">
           <div className="hero-title">
             <h1>React UI</h1>
-            <h2>React组件库，样式基于yahoo的<a href="http://purecss.io/">purecss</a>。</h2>
+            <h2>React组件库，样式基于yahoo的<a target="_blank" href="http://purecss.io/">purecss</a> / <a target="_blank" href="http://pure-site.ap01.aws.af.cm/">中文版</a>。</h2>
             <div style={{marginTop: 40}}>
               <a style={{marginRight: 20}} href="#/build" className="rct-button button-large button-extend rct-button-success">生成组件</a>
-              <a className="rct-button button-large button-extend" href="https://github.com/Lobos/react-ui"><Icon icon="github" /> Github</a>
+              <a target="_blank" className="rct-button button-large button-extend" href="https://github.com/Lobos/react-ui"><Icon icon="github" /> Github</a>
             </div>
           </div>
         </div>
